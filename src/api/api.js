14bedc7fa8e1b4c3de62a0bc70a9a26c65aa8868
FerryAndikaFirmansyah <164197519+FerryAndikaFirmansyah@users.noreply.github.com>
2025-11-12 02:46:08 +0700

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'petshopzn-be2-production.up.railway.app',
     headers: {
         'Content-Type': 'application/json'
     },
